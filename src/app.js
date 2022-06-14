67870dbd0f2c7b8e5aa4e4c134c0729643b96d04
Bhuvnesh Jindal <bhuvneshjindal13@gmail.com>
2022-06-14 13:10:31 +0530

    port changed

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 //hbs.registerPartials(path.join(__dirname, '../partials'))
 
 //app.set('view engine', 'hbs')
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.get('', (req, res) => {
     res.send('index')
 })
